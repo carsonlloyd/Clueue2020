2cce5add5336e5002ea577d0f0a1b43ce32d1451
Ammon <ammon.hepworth@jhuapl.edu>
2020-03-31 02:31:27 -0400

    Add basic network functionality, very ugly/unwieldy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from Board import Board
 from Player import Player
-import time, os, random, argparse, socket, selectors
+import time, os, random, argparse, socket, selectors, types
 
 #Globals
 mainBoard = None
@@ -9,24 +9,22 @@ validInputs = ['up','down','left','right','secret','suggest','accuse']
 playerNames = ['Col. Mustard','Miss Scarlet','Prof. Plum','Mrs. Peacock','Mr. Green','Mrs. White']
 ADDR = 'localhost'
 PORT = 65432
+server_socket = None
 selector = selectors.DefaultSelector()
 
 
 def initNetwork():
-    global HOST, ADDR, PORT, selector
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        if HOST:
-            s.bind((ADDR, PORT))
-            s.listen()
-            print('listening on ', (ADDR,PORT))
-            s.setblocking(False)
-            sel.register(s,selectors.EVENT_READ, data=None)
-            conn, addr = s.accept
-            print('server started, waiting for clients...')
-        else:
-            s.connect((ADDR, PORT))
-
-
+    global HOST, ADDR, PORT, selector, server_socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    if HOST:
+        server_socket.bind((ADDR, PORT))
+        server_socket.listen()
+        print('listening on ', (ADDR,PORT))
+        server_socket.setblocking(False)
+        selector.register(server_socket,selectors.EVENT_READ, data=None)
+        print('server started, waiting for clients...')
+    else:
+        start_connections(ADDR,PORT)
 
 def initPlayers(numPlayers):
     players = [Player(playerNames[i]) for i in random.sample(range(6),numPlayers)]
@@ -49,10 +47,55 @@ def initialize():
     initPlayers(4)
     mainBoard.updatePlayerLocationsOnBoard()
 
-    
-
     #render inital gameboard
     render()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept() #should be ready to read
+    print('accepted connection from: ', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(connid=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    selector.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+        if recv_data:
+            print('received' + repr(recv_data))
+            data.outb += recv_data
+        else:
+            print('closing connection to: ', data.addr)
+            selector.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if not HOST and not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print('sending', repr(data.outb), 'to', data.connid)
+            sent = sock.send(data.outb) # Should be ready to write
+            data.outb = data.outb[sent:]
+    else:
+        print('no mask')
+
+def start_connections(host, port, num_conns=1):
+    messages = [b'message 1', b'message 2']
+    server_addr = ('localhost', 65432)
+    for i in range(0, num_conns):
+        connid = i+1
+        print('starting connection,' + str(connid) + 'to' + str(server_addr))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(connid=connid,
+                                     msg_total = sum(len(m) for m in messages),
+                                     recv_total=0,
+                                     messages=list(messages),
+                                     outb=b'')
+        selector.register(sock, events, data=data)
 
 def getInput():
     '''
@@ -66,14 +109,22 @@ def getInput():
     while action not in validInputs:
         action = input('please select an action (up, down, left, right, secret, accuse, suggest): ')
 
+    events = selector.select(timeout=1)
     #Host network code to accept player input messages
     if HOST:
-        events = selector.select(timeout=None)
         for key, mask in events:
             if key.data is None:
                 accept_wrapper(key.fileobj)
             else:
                 service_connection(key, mask)
+    else:
+        #Client code
+        if events:
+            for key, mask in events:
+                service_connection(key, mask)
+        #check for a socket being monitored to continue
+        if not selector.get_map():
+            print('something probably broke')
 
 
 
@@ -113,6 +164,8 @@ def main():
         getInput()
         update()
         render()
+
+    selector.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
