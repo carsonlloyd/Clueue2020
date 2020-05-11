f8892a924a1e5dcca747e0ddaf029140aac9b19e
Selena Snyder <56155845+stsnyde@users.noreply.github.com>
2020-05-11 01:00:01 -0400

    Adds movement for gameboard and start menu
    Main.py 
    •	Initplayers – used for sending player choice messages and processing selected choice on start menu
    o	Need third value in Message.send_character_choice
    o	Need send_id for Message.send-charaacter_unavail
    
    Board.py
    •	Boardgame for starting game in init (puts in homesquares) – have added valid moves to room.py and movements for home squares.  So could originate in squares and first turn is just moving out of homesquares
    •	Movement in updatePlayerLocationsonBoard
    o	Hopefully lines up on board.  Should take 150 on x axis and 105 on y axis to move between rooms/hallways
    o	Assumes that player variable is ‘G’, ‘W’, ‘M’, etc.

--- Board.py
+++ Board.py
@@ -3,87 +3,88 @@ import os, pygame
 
 class Board:
 
-    def __init__(self):
+    def __init__(self, DISPLAYSURF, PLAYERIMAGES):
         # Creates a brand new, blank board data structure
 
-        DISPLAYSURF = pygame.display.set_mode((960, 720))
-        pygame.display.set_caption('Clueless')
-        black = (0,0,0)
-        white = (255,255,255)
+        global boardimg, images
         boardimg = pygame.image.load('gameboard.png')
-        greenimg = pygame.image.load('green.png')
-        mustardimg = pygame.image.load('mustard.png')
-        scarlettimg = pygame.image.load('scarlett.png')
-        plumimg = pygame.image.load('plum.png')
-        whiteimg = pygame.image.load('white.png')
-        peacockimg = pygame.image.load('peacock.png')
+ 
+        images = {'G': PLAYERIMAGES[0], 'M': PLAYERIMAGES[1], 'S': PLAYERIMAGES[2],'W': PLAYERIMAGES[3], 'P': PLAYERIMAGES[4], 'C': PLAYERIMAGES[5]}
         
-        DISPLAYSURF.fill(white)
         DISPLAYSURF.blit(boardimg, (0,0))
-        DISPLAYSURF.blit(greenimg, (295,650))
-        DISPLAYSURF.blit(whiteimg, (605,650))
-        DISPLAYSURF.blit(peacockimg, (20,450))
-        DISPLAYSURF.blit(plumimg, (20,230))
-        DISPLAYSURF.blit(scarlettimg, (605,20))
-        DISPLAYSURF.blit(mustardimg, (900,230))
+        DISPLAYSURF.blit(PLAYERIMAGES[0], (295,650))
+        gstartx = 295
+        gstarty = 650
+        DISPLAYSURF.blit(PLAYERIMAGES[3], (605,650))
+        wstartx = 605
+        wstarty = 650
+        DISPLAYSURF.blit(PLAYERIMAGES[5], (20,450))
+        cstartx = 20
+        cstarty = 450
+        DISPLAYSURF.blit(PLAYERIMAGES[4], (20,230))
+        pstartx = 20
+        pstarty = 230
+        DISPLAYSURF.blit(PLAYERIMAGES[2], (605,20))
+        sstartx = 605
+        sstarty = 20
+        DISPLAYSURF.blit(PLAYERIMAGES[1], (900,230))
+        mstartx = 900
+        mstarty = 230
 
         self.board = DISPLAYSURF
         self.rooms = [Room.Room(roomType) for roomType in Room.RoomType]
         self.weapons = [w.value for w in Room.WeaponType]
+        self.avatarposx = {'G': gstartx, 'W': wstartx, 'C': cstartx, 'P': pstartx, 'M': mstartx, 'S':sstartx}
+        self.avatarposy = {'G': gstarty, 'W': wstarty, 'C': cstarty, 'P': pstarty, 'M': mstarty, 'S':sstarty}
 
 
-    def draw(self):
-        fullboard = ''
-        print(fullboard)
-        for item in self.board:
-            #print(item)
-            fullboard += item
-        print(fullboard)
 
-    def updatePlayerLocationsOnBoard(self):
+    def draw(self, img, x,y):
+        self.board.blit(img, (x,y))
+
+    def updatePlayerLocationsOnBoard(self, img):
         '''
         updates the game board to properly display where the
         players currently are
         Works by making a list of the characters symbols and then padding to the desired size
-        #TODO: make this work for hallways as well
         '''
-        spacer = '|'
+        self.board.blit(boardimg,(0,0))
+        
+        #draw in rooms
         for i in range(3):
-            idx = 2 + 8*i
-            self.board[idx] = '|'
             for j in range(3):
                 playerString = ' '
                 for player in self.rooms[3*i+j].getPlayers():
                     playerString += str(player) + ' '
-                self.board[idx] += str.center(playerString,12)
-                self.board[idx] += spacer
-                if (j<2):
-                    playerString = ' '
-                    for player in self.rooms[5*i+10+j].getPlayers():
-                        playerString += str(player) + ' '
-                    self.board[idx] += str.center(playerString,10)
-                    self.board[idx] += spacer
-            self.board[idx] += '\n'
-
-        spacer = '|                     |'
+                    if 'up' in action:
+                         self.avatarposy[str(player)] = self.avatarposy[str(player)] + 105
+                    elif 'down' in action:
+                         self.avatarposy[str(player)] = self.avatarposy[str(player)] - 105
+                    elif 'left' in action:
+                         self.avatarposx[str(player)] = self.avatarposx[str(player)] - 150
+                    elif 'right' in action:
+                         self.avatarposx[str(player)] = self.avatarposx[str(player)] + 150
+                   
+                    self.board.blit(images[str(player)], (self.avatarposx[str(player)], self.avatarposy[str(player)]))
+         
+        #draw in hallways 
         for i in range(2):
-            idx = 6 + 8*i
-            self.board[idx] = '      |'
-            for j in range(3):
+           for j in range(3):
                 playerString = ''
                 playerInHallway = False
                 for player in self.rooms[5*i+12+j].getPlayers():
                     playerString += str(player)
                     playerInHallway = True
-                if (playerInHallway):
-                    self.board[idx] += playerString
-                else:
-                    self.board[idx] += ' '
-                if(j<2):
-                    self.board[idx] += spacer
-                else:
-                    self.board[idx] += '|'
-            self.board[idx] += '\n'
+                    if (playerInHallway):
+                         if 'up' in action:
+                             self.avatarposy[str(player)] = self.avatarposy[str(player)] + 105
+                         elif 'down' in action:
+                             self.avatarposy[str(player)] = self.avatarposy[str(player)] - 105
+                         elif 'left' in action:
+                             self.avatarposx[str(player)] = self.avatarposx[str(player)] - 150
+                         elif 'right' in action:
+                             self.avatarposx[str(player)] = self.avatarposx[str(player)] + 150
+                
 
     def getPlayerRoom(self, player):
         for room in self.rooms:
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def addMessage(message, connid):
 
 def accept_wrapper(sock):
     conn, addr = sock.accept() #should be ready to read
-    global playerAddresses, hands
+    global playerAddresses, hands, characters
     playerAddresses[len(playerAddresses)] = addr
     messages[addr] = []
     print('accepted connection from: ', addr)
@@ -124,25 +124,88 @@ def start_connections(host, port, num_conns=1):
 #########################################################################
 # END OF NETWORKING GARBAGE
 ##########################################################################
+players = []
 
-def initPlayers(numPlayers):
+def initPlayers(choice):
     global players
-    players = [Player(playerNames[i]) for i in random.sample(range(6),numPlayers)]
+    #TODO: need final value in send character choice
+    Message.send_character_choice((ADDR,PORT), choice, str(getPlayerBySymbol(choice)))
+    if choice not in players:
+        players.append(choice)
+        Message.send_character_confirm(playerAddresses[turn], choice)
+    else:
+        Message.send_character_unavail(playerAddresses[turn])
+    #players = [Player(playerNames[i]) for i in random.sample(range(6),numPlayers)]
     if HOST:
         for playerIdx,roomIdx in enumerate(random.sample(range(9),numPlayers)):
             mainBoard.rooms[roomIdx].addPlayer(players[playerIdx]) #kinda bad encapsulation but thats pythons fault
 
-def initialize():
+def button(msg,x,y,w,h,ac, action = None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(DISPLAYSURF, ac,(x,y,w,h))
+        if click[0] == 1 and action !=None:
+                action()
+    else:
+        pygame.draw.rect(DISPLAYSURF, ac,(x,y,w,h))
+
+    smallText = pygame.font.Font("freesansbold.ttf",20)
+    textSurf, textRect = text_objects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    DISPLAYSURF.blit(textSurf, textRect)
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (0,0,0))
+    return textSurface, textSurface.get_rect()
+
+def game_intro(DISPLAYSURF, clock):
+
+    scarlett = (200,0,0)
+    white = (255,255,255)
+    green = (0,200,0)
+    peacock = (65,85,150)
+    mustard = (255, 219, 88)
+    plum = (221,160,221)
+
+    intro = True
+    while intro:
+        for event in pygame.event.get():
+            print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        largeText = pygame.font.Font('freesansbold.ttf',50)
+        TextSurf, TextRect = text_objects("Clueless: Pick your character", largeText)
+        TextRect.center = ((960/2),(720/3))
+        DISPLAYSURF.blit(TextSurf, TextRect)
+
+        mouse = pygame.mouse.get_pos()
+
+        button ("Mr. Green", 150, 350, 150, 50, green, initPlayers('G'))
+        button ("Col. Mustard", 150, 450, 150, 50, mustard, initPlayers('M'))
+        button ("Miss Scarlett", 150, 550, 150, 50, scarlett, initPlayers('S'))
+        button ("Prof. Plum", 450, 350, 150, 50, plum, initPlayers('P'))
+        button ("Mrs. Peacock", 450, 450, 150, 50, peacock, initPlayers('C'))
+        button ("Mrs. White", 450, 550, 150, 50, white, initPlayers('W'))
+
+        pygame.display.update()
+        clock.tick(15)
+
+def initialize(DISPLAYSURF, PLAYERIMAGES, clock):
     '''
     Initialize game board
     '''
     global mainBoard
     print('initializing')
 
-    mainBoard = Board()
-    #randomly place players
-    initPlayers(numPlayers)
     initNetwork(numPlayers)
+    mainBoard = Board(DISPLAYSURF, PLAYERIMAGES)
+    #randomly place players
+    #initPlayers(numPlayers)
+
 
 
 def determineOrder():
@@ -544,7 +607,7 @@ def update(action):
             if message:
                 parseMessage(message)
 
-    mainBoard.updatePlayerLocationsOnBoard()
+    mainBoard.updatePlayerLocationsOnBoard(action)
 
 def render():
     '''
@@ -573,8 +636,35 @@ def main():
     Update will change the game state (or process state update message as client)
     Render will draw all graphical items based on that state (no render for host)
     '''
+    global clock, DISPLAYSURF, black, white, IMAGESDICT
     pygame.init()
-    initialize()
+    clock = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((960, 720))
+    pygame.display.set_caption('Clueless')
+    black = (0,0,0)
+    white = (255,255,255)
+    DISPLAYSURF.fill(white)
+
+    IMAGESDICT = {'gameboard': pygame.image.load('gameboard.png'),
+                  'greenplayer': pygame.image.load('green.png'),
+                  'mustardplayer': pygame.image.load('mustard.png'),
+                  'scarletplayer': pygame.image.load('scarlett.png'),
+                  'whiteplayer': pygame.image.load('white.png'),
+                  'plumplayer': pygame.image.load('plum.png'),
+                  'peacockplayer': pygame.image.load('peacock.png')}
+
+    currentImage = 0
+    PLAYERIMAGES = [IMAGESDICT['greenplayer'],
+                    IMAGESDICT['mustardplayer'],
+                    IMAGESDICT['scarletplayer'],
+                    IMAGESDICT['whiteplayer'],
+                    IMAGESDICT['plumplayer'],
+                    IMAGESDICT['peacockplayer']]
+
+    #game_intro(DISPLAYSURF, clock)
+
+    initialize(DISPLAYSURF, PLAYERIMAGES, clock)
+
 
     global game_won
     while not game_won:
@@ -582,6 +672,7 @@ def main():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+                game_won = True
         pygame.display.update()
 
     #global game_won
